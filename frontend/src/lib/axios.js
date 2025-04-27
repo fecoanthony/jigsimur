2e810299ab515a346f41fda7510fae7ea6f7ff89
anthony <fecotony@gmail.com>
2025-04-27 19:16:15 +0100

    axios baseURL modifid

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
+import dotenv from "dotenv"
+dotenv.config()
 
 const axiosInstance = axios.create({
 	// baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-	baseURL: "http://localhost:5000/api",
+	// baseURL: "http://localhost:5000/api",
+	baseURL: "https://jigsimur-2.onrender.com/api",
 	withCredentials: true, // send cookies to the server
 });
 
